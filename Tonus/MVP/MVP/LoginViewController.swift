408cae8669def85686ae48b7404052aade56b0a5
Roh Krishnan <rohkrishnan@Rohits-MacBook-Pro.local>
2017-05-04 18:21:42 -0700

    MVP, compiled, basic login view --need to add button functionality

---
+++ Tonus/MVP/MVP/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Roh Krishnan on 5/4/17.
+//  Copyright © 2017 Roh Krishnan. All rights reserved.
+//
+
+import UIKit
+
+
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+    
+
+
+
+
