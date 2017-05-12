94a1f91fe6fb8d4c930868395d1c7f1678a1ca9b
Roh Krishnan <rohkrishnan@rohits-mbp.attlocal.net>
2017-05-12 12:51:04 -0700

    added tonus images and a new view on storyboard for query

---
+++ MVP/FirebaseTutorial/DashboardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DashboardViewController.swift
+//  FirebaseTutorial
+//
+//  Created by Roh Krishnan on 5/10/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import Foundation
---
+++ Skeleton_App/socialshare/QueryCreationView.swift
@@ -0,0 +1,9 @@
+//
+//  QueryCreationView.swift
+//  socialshare
+//
+//  Created by Roh Krishnan on 5/11/17.
+//  Copyright © 2017 MyAppTemplates. All rights reserved.
+//
+
+import Foundation
